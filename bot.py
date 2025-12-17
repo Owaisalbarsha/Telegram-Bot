44e59adfed2b02c06c60f68045708b394d7d9414
Owaisalbarsha <owaisalbarsha828@gmail.com>
2025-12-17 23:48:27 +0300

    Initial commit: Telegram bot - test bot

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update, context):
+    await update.message.reply_text("Hi Nigga 👦🏿")
+
+async def echo(update, context):
+    await update.message.reply_text(update.message.text)
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
---
+++ bot2.py
@@ -0,0 +1,11 @@
+import os
+import telebot
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(command=['Greet'])
+def greet(message):
+    bot.reply_to(message, "Hey! Hows it going?")
+
+bot.polling()
